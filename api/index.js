e6902bf68c6f44b0027b340eaa4fb9a04d65510f
Ujmalik56 <umairattaa3@gmail.com>
2025-11-20 15:40:45 +0500

    Delete api/index.js

--- api/index.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.statusCode = 301;
-  res.setHeader('Location', 'https://google.com');
-  res.setHeader('Cache-Control', 'public, max-age=86400');
-  res.end();
-}
