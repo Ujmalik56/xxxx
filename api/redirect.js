4cd7d32037e28e59116fee85d2d760887229fce5
Ujmalik56 <umairattaa3@gmail.com>
2025-11-20 15:26:38 +0500

    Delete api/redirect.js

--- api/redirect.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  // Permanent redirect - no page visible
-  res.writeHead(301, {
-    'Location': 'https://yt.webbloggin.online/nevir/',
-    'Cache-Control': 'public, max-age=31536000'
-  });
-  res.end();
-}
